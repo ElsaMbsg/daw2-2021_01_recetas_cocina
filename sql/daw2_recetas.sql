bed61ce2ccf411e94a26197b7c2a7ac57ead2e1e
mluevi17 <mluevi17@usal.es>
2022-01-05 17:09:27 +0100

    Modificado tienda para seleccionar usuario tienda. Modificada reparticion de tareas. Modificado archivo de BBDD.

--- sql/daw2_recetas.sql
+++ sql/daw2_recetas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-01-2022 a las 19:18:51
+-- Tiempo de generación: 05-01-2022 a las 17:06:29
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `daw2_recetas`
 --
+CREATE DATABASE IF NOT EXISTS `daw2_recetas` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `daw2_recetas`;
 
 -- --------------------------------------------------------
 
@@ -52,13 +54,74 @@ CREATE TABLE `ingredientes` (
 --
 
 INSERT INTO `ingredientes` (`id`, `nombre`, `descripcion`, `datos_nutricion`) VALUES
-(1, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento. Ingrediente Condimento.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(2, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.vegetariano.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(3, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(4, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(5, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(6, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.', 'Composición	Cantidad (gr)	CDR(%)\r\nKcalorías	0	0%\r\nCarbohidratos	0	0%\r\nProteínas	0	0%\r\nFibra	0	0%\r\nGrasas	0	0%'),
-(7, 'Sal', 'Nombre común del cloruro sódico, sustancia blanca, cristalina, de sabor acre y muy soluble en agua, que se emplea como condimento.', 'Valor calórico\r\n0 kcal\r\n0 kJ\r\nGrasas\r\n0,0 g\r\nGrasas saturadas\r\n0,0 g\r\nGrasas monoinsaturadas\r\n0,0 g\r\nGrasas poliinsaturadas\r\n0,0 g\r\nCarbohidratos\r\n0,0 g\r\nAzúcares\r\n0,0 g\r\nProteínas\r\n0,0 g\r\nFibra alimentaria\r\n0,0 g\r\nColesterol\r\n0,0 mg\r\nSodio\r\n1,0 g\r\nAgua\r\n< 0,1 g');
+(1, '  Aceite de girasol', 'Ingrediente Condimento', '144kcal'),
+(10, '              Huevo', 'Ingrediente Proteico', '144kcal'),
+(11, '     Yogur de limón', 'Ingrediente Postre', '144kcal'),
+(12, '    Aceite de oliva', 'Ingrediente Condimento', '144kcal'),
+(13, '             Azúcar', 'Ingrediente Condimento', '144kcal'),
+(14, '             Harina', 'Ingrediente Condimento', '144kcal'),
+(15, '           Levadura', 'Ingrediente Condimento', '144kcal'),
+(16, '              Limón', 'Ingrediente Fruta', '144kcal'),
+(17, '        Azúcar glas', 'Ingrediente Condimento', '144kcal'),
+(18, '        Mantequilla', 'Ingrediente Condimento', '144kcal'),
+(19, '           Lentejas', 'Ingrediente Legumbre', '144kcal'),
+(20, '         Zanahorias', 'Ingrediente Verdura', '144kcal'),
+(21, '                Ajo', 'Ingrediente Condimento', '144kcal'),
+(22, '            Cebolla', 'Ingrediente Condimento', '144kcal'),
+(23, '             Laurel', 'Ingrediente Condimento', '144kcal'),
+(24, '     Pimentón dulce', 'Ingrediente Condimento', '144kcal'),
+(25, '                Sal', 'Ingrediente Condimento', '144kcal'),
+(26, '     Pimienta negra', 'Ingrediente Condimento', '144kcal'),
+(27, '     Pimiento verde', 'Ingrediente Verdura', '144kcal'),
+(28, '             Tomate', 'Ingrediente Verdura', '144kcal'),
+(29, '      Hueso de caña', 'Ingrediente Carne', '144kcal'),
+(30, '            Chorizo', 'Ingrediente Carne', '144kcal'),
+(31, '           Morcilla', 'Ingrediente Carne', '144kcal'),
+(32, '     Hueso de jamón', 'Ingrediente carne', '144kcal'),
+(33, '               Agua', 'Ingrediente Bebida', '144kcal'),
+(34, '        Arroz bomba', 'Ingrediente Pasta', '144kcal'),
+(35, '            Plátano', 'Ingrediente Fruta', '144kcal'),
+(36, '        Tomate pera', 'Ingrediente Verdura', '144kcal'),
+(37, '     Pimiento verde', 'Ingrediente Verdura', '144kcal'),
+(38, '     Alubias negras', 'Ingrediente Legumbre', '144kcal'),
+(39, '       Chile molido', 'Ingrediente Carne', '144kcal'),
+(41, '      Pimiento rojo', 'Ingrediente Verdura', '144kcal'),
+(42, ' Tortillas de trigo', 'Ingrediente Vegetariano', '144kcal'),
+(43, '            Patatas', 'Ingrediente Verdura', '144kcal'),
+(44, '          Calabacín', 'Ingrediente Verdura', '144kcal'),
+(45, '          Berenjena', 'Ingrediente Verdura', '144kcal'),
+(46, '        Champiñones', 'Ingrediente Vegano', '144kcal'),
+(47, '              Jamón', 'Ingrediente Conserva', '144kcal'),
+(48, '    Fideos de arroz', 'Ingrediente Pasta', '144kcal'),
+(49, '         Tofu firme', 'Ingrediente Vegetariano', '144kcal'),
+(50, '    Jengibre fresco', 'Ingrediente Condimento', '144kcal'),
+(51, '         Salsa soja', 'Ingrediente Salsa', '144kcal'),
+(52, '       Curry molido', 'Ingrediente Condimento', '144kcal'),
+(53, '      Ajo granulado', 'Ingrediente Condimento', '144kcal'),
+(54, '     Cúrcuma molida', 'Ingrediente Condimento', '144kcal'),
+(55, '               Lima', 'Ingrediente Fruta', '144kcal'),
+(56, '     Perejil fresco', 'Ingrediente Condimento', '144kcal'),
+(57, '           Cilantro', 'Ingrediente Condimento', '144kcal'),
+(58, '   Pechuga de pollo', 'Ingrediente Carne', '144kcal'),
+(59, '        Vino blanco', 'Ingrediente Bebida', '144kcal'),
+(111, '            Maicena', 'Ingrediente Condimento', '144kcal'),
+(112, '            Kétchup', 'Ingrediente Salsa', '144kcal'),
+(113, '   Vinagre de arroz', 'Ingrediente Condimento', '144kcal'),
+(114, '      Azúcar moreno', 'Ingrediente Condimento', '144kcal'),
+(115, '          Solomillo', 'Ingrediente Carne', '144kcal'),
+(116, '               Lomo', 'Ingrediente Carne', '144kcal'),
+(117, '           Shiratak', 'Ingrediente Pasta', '144kcal'),
+(118, '           Shungiku', 'Ingrediente Condimento', '144kcal'),
+(119, '    Brotes de bambú', 'Ingrediente Vegetariano', '144kcal'),
+(120, '               Café', 'Ingrediente Bebida', '144kcal'),
+(121, '\'Caldo Japonés dashi', 'Ingrediente Bebida', '144kcal'),
+(122, '      Miel de arroz', 'Ingrediente Condimento', '144kcal'),
+(123, '       Arroz blanco', 'Ingrediente Pasta', '144kcal'),
+(124, '            Caballa', 'Ingrediente Pescado', '144kcal'),
+(125, '          Mugi miso', 'Ingrediente Vegano', '144kcal'),
+(126, '     Miso de cebada', 'Ingrediente Vegano', '144kcal'),
+(127, '         Shiro miso', 'Ingrediente Vegetariano', '144kcal'),
+(128, '        Miso blanco', 'Ingrediente Vegetariano', '144kcal');
 
 -- --------------------------------------------------------
 
@@ -189,6 +252,13 @@ CREATE TABLE `receta_ingredientes` (
   `notas` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `receta_ingredientes`
+--
+
+INSERT INTO `receta_ingredientes` (`id`, `receta_id`, `ingrediente_id`, `cantidad`, `medida`, `notas`) VALUES
+(1, 6, 10, 2, 'unidades', '');
+
 -- --------------------------------------------------------
 
 --
@@ -307,9 +377,7 @@ CREATE TABLE `tiendas` (
 
 INSERT INTO `tiendas` (`id`, `nombre`, `domicilio`, `poblacion`, `provincia`, `usuario_id`, `activa`, `visible`) VALUES
 (1, 'Alimerka', 'Dr. Fleming, 1', 'Zamora', 'Zamora', 2, 1, 1),
-(2, 'Carrefour Express', 'Ctra. Pueblica, 3', 'Tábara', 'Zamora', 3, 1, 1),
-(5, 'Coviran', 'Ctra. las Batuecas Nº 2', 'La Alberca', 'Salamanca', 4, 1, 1),
-(6, 'asfasf', 'asfasf', 'asfasf', 'A Coruña', 1, 1, 1);
+(2, 'Carrefour Express', 'Ctra. Pueblica, 3', 'Tábara', 'Zamora', 3, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -327,21 +395,6 @@ CREATE TABLE `tienda_ofertas` (
   `medida` varchar(45) DEFAULT NULL,
   `notas` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tienda_ofertas`
---
-
-INSERT INTO `tienda_ofertas` (`id`, `tienda_id`, `ingrediente_id`, `descripcion`, `envase`, `cantidad`, `medida`, `notas`) VALUES
-(1, 1, 1, 'Sal del Alimerka. Vegetariano.', 'Envase Plastico ', 1, 'Kg', '1€'),
-(2, 2, 4, 'Sal del himalaya.carne.', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(3, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(4, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(5, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(6, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(7, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(8, 2, 4, 'Sal del himalaya', 'Bolsa de plastico', 1, 'Kg', '1.25€'),
-(9, 1, 5, 'prueba', 'prueba', 1, 'Kg', '23€');
 
 -- --------------------------------------------------------
 
@@ -478,7 +531,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `ingredientes`
 --
 ALTER TABLE `ingredientes`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- AUTO_INCREMENT de la tabla `menus`
@@ -526,7 +579,7 @@ ALTER TABLE `receta_comentarios`
 -- AUTO_INCREMENT de la tabla `receta_ingredientes`
 --
 ALTER TABLE `receta_ingredientes`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `receta_pasos`
@@ -544,13 +597,13 @@ ALTER TABLE `receta_paso_imagenes`
 -- AUTO_INCREMENT de la tabla `tiendas`
 --
 ALTER TABLE `tiendas`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tienda_ofertas`
 --
 ALTER TABLE `tienda_ofertas`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
